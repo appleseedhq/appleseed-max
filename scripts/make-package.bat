d86df0f061e14ee11158aa9690624edb1625cd7a
Francois Beaune <beaune@aist.enst.fr>
2016-04-26 09:12:44 +0200

    Omit VC++ redistributable from package
    They should already be installed in the user machine by 3ds Max's installer.

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -1,6 +1,3 @@
 @echo off
-
-copy /Y "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x64\Microsoft.VC110.CRT\*.*" ..\bin\x64\Ship\
 tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy.zip ..\bin\x64\Ship\*.*
-
 pause
