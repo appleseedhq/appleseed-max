8315a000d77087d07fe630b764a02c4c35ee763d
Franz Beaune <franz@appleseedhq.net>
2016-09-19 10:39:09 +0200

    Improve packaging scripts

--- scripts/make-package-debug.bat
+++ scripts/make-package-debug.bat
@@ -1,4 +1,24 @@
 @echo off
-tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy-debug.zip ..\bin\appleseed-max2015\x64\Debug\*.*
-tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy-debug.zip ..\bin\appleseed-max2017\x64\Debug\*.*
+
+pushd %~dp0
+
+REM Cleanup
+rmdir /S /Q appleseed 2>nul
+del appleseed-max2015-x.x.x-yyyy-debug.zip 2>nul
+del appleseed-max2017-x.x.x-yyyy-debug.zip 2>nul
+
+REM 3ds Max 2015 archive
+mkdir appleseed
+copy ..\bin\appleseed-max2015\x64\Debug\*.* appleseed
+tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy-debug.zip appleseed
+rmdir /S /Q appleseed
+
+REM 3ds Max 2017 archive
+mkdir appleseed
+copy ..\bin\appleseed-max2017\x64\Debug\*.* appleseed
+tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy-debug.zip appleseed
+rmdir /S /Q appleseed
+
+popd
+
 pause
--- scripts/make-package-ship.bat
+++ scripts/make-package-ship.bat
@@ -1,4 +1,24 @@
 @echo off
-tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy.zip ..\bin\appleseed-max2015\x64\Ship\*.*
-tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy.zip ..\bin\appleseed-max2017\x64\Ship\*.*
+
+pushd %~dp0
+
+REM Cleanup
+rmdir /S /Q appleseed 2>nul
+del appleseed-max2015-x.x.x-yyyy.zip 2>nul
+del appleseed-max2017-x.x.x-yyyy.zip 2>nul
+
+REM 3ds Max 2015 archive
+mkdir appleseed
+copy ..\bin\appleseed-max2015\x64\Ship\*.* appleseed
+tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy.zip appleseed
+rmdir /S /Q appleseed
+
+REM 3ds Max 2017 archive
+mkdir appleseed
+copy ..\bin\appleseed-max2017\x64\Ship\*.* appleseed
+tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy.zip appleseed
+rmdir /S /Q appleseed
+
+popd
+
 pause
