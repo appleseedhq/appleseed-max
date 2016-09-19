583a866c16b855f7d447d1e34197729f5ded085e
Franz Beaune <franz@appleseedhq.net>
2016-09-19 09:03:40 +0200

    Update packaging scripts

--- scripts/make-package-debug.bat
+++ scripts/make-package-debug.bat
@@ -1,3 +1,4 @@
 @echo off
-tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy-debug.zip ..\bin\x64\Debug\*.*
+tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy-debug.zip ..\bin\appleseed-max2015\x64\Debug\*.*
+tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy-debug.zip ..\bin\appleseed-max2017\x64\Debug\*.*
 pause
--- scripts/make-package-ship.bat
+++ scripts/make-package-ship.bat
@@ -1,3 +1,4 @@
 @echo off
-tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy.zip ..\bin\x64\Ship\*.*
+tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy.zip ..\bin\appleseed-max2015\x64\Ship\*.*
+tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy.zip ..\bin\appleseed-max2017\x64\Ship\*.*
 pause
