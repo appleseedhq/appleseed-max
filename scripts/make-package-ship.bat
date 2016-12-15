7c70d50a1b571ecbdfe3e7af0a0acd26c290995d
Franz Beaune <franz@appleseedhq.net>
2016-12-15 13:42:24 +0100

    Tweak packaging scripts

--- scripts/make-package-ship.bat
+++ scripts/make-package-ship.bat
@@ -2,22 +2,25 @@
 
 pushd %~dp0
 
-REM Cleanup
+echo ============ Cleaning up ============
 rmdir /S /Q appleseed 2>nul
 del appleseed-max2015-x.x.x-yyyy.zip 2>nul
 del appleseed-max2017-x.x.x-yyyy.zip 2>nul
+echo.
 
-REM 3ds Max 2015 archive
+echo ============ Creating 3ds Max 2015 archive ============
 mkdir appleseed
 copy ..\bin\appleseed-max2015\x64\Ship\*.* appleseed
-tools\7z\7z.exe a -r -mx9 appleseed-max2015-x.x.x-yyyy.zip appleseed
+tools\7z\7z.exe a -r -mx=9 appleseed-max2015-x.x.x-yyyy.zip appleseed
 rmdir /S /Q appleseed
+echo.
 
-REM 3ds Max 2017 archive
+echo ============ Creating 3ds Max 2017 archive ============
 mkdir appleseed
 copy ..\bin\appleseed-max2017\x64\Ship\*.* appleseed
-tools\7z\7z.exe a -r -mx9 appleseed-max2017-x.x.x-yyyy.zip appleseed
+tools\7z\7z.exe a -r -mx=9 appleseed-max2017-x.x.x-yyyy.zip appleseed
 rmdir /S /Q appleseed
+echo.
 
 popd
 
