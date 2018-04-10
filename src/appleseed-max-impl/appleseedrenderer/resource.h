0b3d15c1a915a3edc23315ff824b178b1bdcd493
Sergo <usakhelo@users.noreply.github.com>
2018-04-09 20:29:47 -0400

    Allow selecting everything in the Log window (issue #153)

--- src/appleseed-max-impl/appleseedrenderer/resource.h
+++ src/appleseed-max-impl/appleseedrenderer/resource.h
@@ -53,14 +53,15 @@
 #define IDC_STATIC_LOG                              602
 #define IDC_BUTTON_LOG                              603
 #define IDC_EDIT_LOG                                604
+#define ID_ACCELERATOR_LOG_CTRL_A                   40001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        110
-#define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_COMMAND_VALUE         40003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
