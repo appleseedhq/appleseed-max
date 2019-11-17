fe54e7ac960199e11e51c3e84e766e7669955ccb
Mango-3 <numen22@hotmail.com>
2019-11-17 20:13:22 +0200

    Cosmetics

--- src/appleseed-max-impl/appleseedobjpropsmod/appleseedobjpropsmod.cpp
+++ src/appleseed-max-impl/appleseedobjpropsmod/appleseedobjpropsmod.cpp
@@ -5,7 +5,7 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2017-2018 Francois Beaune, The appleseedhq Organization
+// Copyright (c) 2017-2019 Francois Beaune, The appleseedhq Organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -375,6 +375,7 @@ float AppleseedObjPropsMod::get_shadow_terminator_correction(const TimeValue t) 
 {
     return m_pblock->GetFloat(ParamIdShadowTerminatorCorrection, t, FOREVER);
 }
+
 
 //
 // AppleseedObjPropsModClassDesc class implementation.
