d7a72f1f34c07015d04ad1b488b991831f2464de
Franz Beaune <franz@appleseedhq.net>
2019-11-30 10:07:39 +0100

    Resource IDs cleanups

--- src/appleseed-max-impl/appleseedobjpropsmod/resource.h
+++ src/appleseed-max-impl/appleseedobjpropsmod/resource.h
@@ -2,43 +2,53 @@
 // Microsoft Visual C++ generated include file.
 // Used by appleseedobjpropsmod.rc
 //
-#define IDC_SPINNER_DL_SAMPLES                      329
-#define IDC_CHECK1                                  1005
 #define IDD_FORMVIEW_PARAMS                         7000
 #define IDS_FORMVIEW_PARAMS_TITLE                   7001
+
 #define IDC_BUTTON_VISIBILITY_CAMERA                7010
 #define IDS_VISIBILITY_CAMERA                       7011
+
 #define IDC_BUTTON_VISIBILITY_LIGHT                 7020
 #define IDS_VISIBILITY_LIGHT                        7021
+
 #define IDC_BUTTON_VISIBILITY_SHADOW                7030
 #define IDS_VISIBILITY_SHADOW                       7031
+
 #define IDC_BUTTON_VISIBILITY_TRANSPARENCY          7040
 #define IDS_VISIBILITY_TRANSPARENCY                 7041
+
 #define IDC_BUTTON_VISIBILITY_PROBE                 7050
 #define IDS_VISIBILITY_PROBE                        7051
+
 #define IDC_BUTTON_VISIBILITY_DIFFUSE               7060
 #define IDS_VISIBILITY_DIFFUSE                      7061
+
 #define IDC_BUTTON_VISIBILITY_GLOSSY                7070
 #define IDS_VISIBILITY_GLOSSY                       7071
+
 #define IDC_BUTTON_VISIBILITY_SPECULAR              7080
 #define IDS_VISIBILITY_SPECULAR                     7081
+
 #define IDC_BUTTON_VISIBILITY_SSS                   7090
 #define IDS_VISIBILITY_SSS                          7091
-#define IDC_SSS_SET                                 7092
+#define IDC_TEXTEDIT_SSS_SET                        7092
 #define IDS_SSS_SET                                 7093
 #define IDC_SSS_SET_LABEL                           7094
 #define IDC_STATIC_VISIBILITY                       7095
+
 #define IDC_BUTTON_OPTIMIZE_FOR_INSTANCING          7101
 #define IDS_OPTIMIZE_FOR_INSTANCING                 7102
-#define IDS_STATIC_MEDIUM_PRIORITY                  7200
-#define IDS_TEXT_MEDIUM_PRIORITY                    7201
-#define IDS_SPINNER_MEDIUM_PRIORITY                 7202
-#define IDS_SPINNER_MEDIUM_PRIORITY2                7204
-#define IDC_BUTTON_PHOTON_TARGET                    7300
-#define IDS_PHOTON_TARGET                           7301
-#define IDS_TEXT_SHADOW_TERMINATOR_CORRECTION       7400
-#define IDS_SPINNER_SHADOW_TERMINATOR_CORRECTION    7401
-#define IDC_STATIC_SHADOW_TERMINATOR_CORRECTION     7402
+
+#define IDC_STATIC_MEDIUM_PRIORITY                  7110
+#define IDC_EDIT_MEDIUM_PRIORITY                    7111
+#define IDC_SPINNER_MEDIUM_PRIORITY                 7112
+
+#define IDC_BUTTON_PHOTON_TARGET                    7120
+#define IDS_PHOTON_TARGET                           7121
+
+#define IDC_EDIT_SHADOW_TERMINATOR_CORRECTION       7130
+#define IDC_SPINNER_SHADOW_TERMINATOR_CORRECTION    7131
+#define IDC_STATIC_SHADOW_TERMINATOR_CORRECTION     7132
 
 // Next default values for new objects
 // 
